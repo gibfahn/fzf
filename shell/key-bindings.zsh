80aa1c9e5691e915a81a888b5b68f775c992081a
Gibson Fahnestock <gibfahn@gmail.com>
2020-06-27 22:14:21 +0100

    Allow inserting multiple lines from history
    Allows inserting multiple lines from the history at once.
    
    Preserves the existing behaviour, e.g. moving the cursor to the end of
    the input line.
    
    Newline-separates the lines if multiple are specified. I didn't add a
    trailing newline to keep compatibility with the existing implementation.
    
    Fixes: https://github.com/junegunn/fzf/issues/1806
    Refs: https://superuser.com/questions/1564346/insert-multiple-lines-from-zsh-history

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -105,29 +105,37 @@ if [[ "${FZF_ALT_C_COMMAND-x}" != "" ]]; then
 fi
 
 # CTRL-R - Paste the selected command from history into the command line
+# Supports pasting multiple items.
 fzf-history-widget() {
-  local selected
+  local selected num
   setopt localoptions noglobsubst noposixbuiltins pipefail no_aliases noglob nobash_rematch 2> /dev/null
   # Ensure the module is loaded if not already, and the required features, such
   # as the associative 'history' array, which maps event numbers to full history
   # lines, are set. Also, make sure Perl is installed for multi-line output.
   if zmodload -F zsh/parameter p:{commands,history} 2>/dev/null && (( ${+commands[perl]} )); then
-    selected="$(printf '%s\t%s\000' "${(kv)history[@]}" |
+    # Read history line numbers (split on newline) into selected array.
+    selected=("${(@f)$(printf '%s\t%s\000' "${(kv)history[@]}" |
       perl -0 -ne 'if (!$seen{(/^\s*[0-9]+\**\t(.*)/s, $1)}++) { s/\n/\n\t/g; print; }' |
-      FZF_DEFAULT_OPTS=$(__fzf_defaults "" "-n2..,.. --scheme=history --bind=ctrl-r:toggle-sort --wrap-sign '\t↳ ' --highlight-line ${FZF_CTRL_R_OPTS-} --query=${(qqq)LBUFFER} +m --read0") \
-      FZF_DEFAULT_OPTS_FILE='' $(__fzfcmd))"
+      FZF_DEFAULT_OPTS=$(__fzf_defaults "" "-n2..,.. --scheme=history --bind=ctrl-r:toggle-sort --wrap-sign '\t↳ ' --highlight-line ${FZF_CTRL_R_OPTS-} --query=${(qqq)LBUFFER} -m --read0") \
+      FZF_DEFAULT_OPTS_FILE='' $(__fzfcmd) --print0 | perl -0 -l012 -ne 'print((split("\t", $_))[0])')}")
   else
-    selected="$(fc -rl 1 | awk '{ cmd=$0; sub(/^[ \t]*[0-9]+\**[ \t]+/, "", cmd); if (!seen[cmd]++) print $0 }' |
-      FZF_DEFAULT_OPTS=$(__fzf_defaults "" "-n2..,.. --scheme=history --bind=ctrl-r:toggle-sort --wrap-sign '\t↳ ' --highlight-line ${FZF_CTRL_R_OPTS-} --query=${(qqq)LBUFFER} +m") \
-      FZF_DEFAULT_OPTS_FILE='' $(__fzfcmd))"
+    selected=("${(@f)$(fc -rl 1 | awk '{ cmd=$0; sub(/^[ \t]*[0-9]+\**[ \t]+/, "", cmd); if (!seen[cmd]++) print $0 }' |
+      FZF_DEFAULT_OPTS=$(__fzf_defaults "" "-n2..,.. --scheme=history --bind=ctrl-r:toggle-sort --wrap-sign '\t↳ ' --highlight-line ${FZF_CTRL_R_OPTS-} --query=${(qqq)LBUFFER} -m") \
+      FZF_DEFAULT_OPTS_FILE='' $(__fzfcmd) --print0 | perl -0 -l012 -ne 'print((split("\t", $_))[0])')}")
   fi
   local ret=$?
-  if [ -n "$selected" ]; then
-    if [[ $(awk '{print $1; exit}' <<< "$selected") =~ ^[1-9][0-9]* ]]; then
-      zle vi-fetch-history -n $MATCH
-    else # selected is a custom query, not from history
-      LBUFFER="$selected"
-    fi
+  if [[ "${#selected[@]}" -ne 0 ]]; then
+    local -a history_lines=()
+    for num in "${selected[@]}"; do
+      # Add history at line $num to history_lines array.
+      zle vi-fetch-history -n $num
+      history_lines+=("$BUFFER")
+      BUFFER=
+    done
+    # Set input buffer to newline-separated list of history lines.
+    BUFFER="${(F)history_lines}"
+    # Move cursor to end of buffer.
+    CURSOR=$#BUFFER
   fi
   zle reset-prompt
   return $ret
